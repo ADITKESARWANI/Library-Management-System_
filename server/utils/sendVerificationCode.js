032b899554ec977f2ccd5d15adf8c03424bb2326
Aditya Kesarwani <kesarwaniaditya599@gmail.com>
2026-09-20 15:05:17 +0530

    emailchangesuccess

--- server/utils/sendVerificationCode.js
+++ server/utils/sendVerificationCode.js
@@ -1,20 +1,23 @@
 import { generateVerificationOtpEmailTemplate } from "./emailTemplates.js";
 import { sendEmail } from "./sendEmail.js";
 
-export async function sendVerificationCode(verificationCode,email,res){
-    try{
+export async function sendVerificationCode(verificationCode, email, res) {
+    try {
         const message = generateVerificationOtpEmailTemplate(verificationCode);
+
         await sendEmail({
             email,
             subject: "Verification Code (Bookworm Library Management System)",
             message,
         });
-        
+
         res.status(200).json({
             success: true,
             message: "Verification Code sent successfully",
         });
-    }catch (error) {
+    } catch (error) {
+        console.error("Verification email error:", error);
+
         return res.status(500).json({
             success: false,
             message: "Verification Code failed to send.",

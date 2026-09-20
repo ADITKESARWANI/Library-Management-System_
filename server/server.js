b2e4a106084b3eb29b8cde96b1a149f4bac9aa20
Aditya Kesarwani <kesarwaniaditya599@gmail.com>
2026-09-20 15:20:28 +0530

    Refactor server to use constant for PORT

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ cloudinary.config({
 
 await connectDB();
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`👍 Server is running on port ${process.env.PORT || 5000}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`👍 Server is running on port ${PORT}`);
 });
